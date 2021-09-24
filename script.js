d4ee67bf6782f15b97f9577ae8f3e3ed9527174b
Julia Barcelos <julaai@gmail.com>
2021-09-24 14:58:25 -0300

    ajuste do requisito 18

--- script.js
+++ script.js
@@ -11,26 +11,31 @@ function login() {
 const entrada = document.querySelector('.entrada');
 entrada.addEventListener('click', login);
 
-// requisito 18 Confere se o check está marcado e desabilita o submit do button caso não esteja. A resposta de "Pranav" no site: https://qastack.com.br/programming/9887360/how-can-i-check-if-a-checkbox-is-checked nos fez pensar em uma saída, mas só depois de ver o código das meninas Meiryland e Camila é que conseguimos acertar o nosso.
-const check = document.querySelector('#agreement');
+// requisito 18 - A resposta de "Pranav" no site: https://qastack.com.br/programming/9887360/how-can-i-check-if-a-checkbox-is-checked e o código do colega João Melo conseguimos arrumar o nosso.
+const checkAgree = document.querySelector('#agreement');
 const submit = document.querySelector('#submit-btn');
-check.addEventListener('click', (event) => {
-  event.target.classList.toggle('checked');
-  if (check.className === 'checked') {
+checkAgree.addEventListener('click', () => {
+  if (checkAgree.checked) {
     submit.disabled = false;
   } else {
     submit.disabled = true;
   }
 });
+// sem if nem nada usar : submit.disabled = !submit.disabled
 
-// taskList.addEventListener('click', (event) => {
+// Pensamos também nessa solução das classes conversando e vendo o código da Camila.
+// const check = document.querySelector('#agreement');
+// const submit = document.querySelector('#submit-btn');
+// check.addEventListener('click', (event) => {
 //   event.target.classList.toggle('checked');
+//   if (check.className === 'checked') {
+//     submit.disabled = false;
+//   } else {
+//     submit.disabled = true;
+//   }
 // });
 
-// perguntar para as meninas sobre o uso do className e classList para acessar esse checked.
-
 // requisito 20
-
 const textArea = document.querySelector('#textarea');
 const count = document.querySelector('#counter');
 textArea.addEventListener('input', () => {
@@ -42,3 +47,102 @@ textArea.addEventListener('input', () => {
     count.innerHTML = result;
   }
 });
+
+// escutar o botão de envio e armazenar nos próprios inputs as informações preenchidas e as solicitações.
+const allForm = document.querySelector('#evaluation-form');
+const clearForm = () => allForm.innerHTML = '';
+
+const family = document.querySelectorAll('.family');
+const allRate = document.querySelectorAll('.rate');
+let inputFamily = '';
+let inputRate = '';
+
+function checkSubjects() {
+  let personSubjects = '';
+  const checkedButtons = document.querySelectorAll('.subject:checked');
+  for (let i = 0; i < checkedButtons.length; i += 1) {
+    personSubjects += checkedButtons[i].value;
+    if (i + 1 !== checkedButtons.length) {
+      personSubjects += ', ';
+    }
+  }
+  return personSubjects;
+}
+
+family.forEach((radio) => {
+  radio.addEventListener('click', (event) => {
+    inputFamily = event.target.value;
+  });
+});
+
+allRate.forEach((rate) => {
+  rate.addEventListener('click', (event) => {
+    inputRate = event.target.value;
+  });
+});
+submit.addEventListener('click', (event) => {
+  event.preventDefault();
+  const formAnswers = (`\n
+  Nome: ${document.querySelector('#input-name').value} ${document.querySelector('#input-lastname').value}
+    Email: ${document.querySelector('#input-email').value}
+    Casa: ${document.querySelector('#house').value}
+    Família: ${inputFamily}
+    Avaliação: ${inputRate}
+    Matéria: ${checkSubjects()}
+    Observações: ${document.querySelector('#textarea').value}`);
+  clearForm()
+  document.querySelector('#evaluation-form').innerHTML = formAnswers;
+});
+
+// Família: ${}
+//   Matérias: ${}
+//   Avaliação: ${}
+// var rates = document.getElementById('rates').value;
+// document.getElementById("rates").rate.value
+
+// requisito 21
+
+// submit.addEventListener('click', (event) => { 
+//   const fullName = `Nome: ${document.querySelector('#input-name')} ${document.querySelector('#input-lastname').value}`;
+//   const email = `Email: ${document.querySelector('#input-email').value}`;
+//   const house = `Casa: ${document.querySelector('#house').value}`;
+//   const family = `Família: ${checkSelected(document.querySelector('#label-family').value)}`;
+//   const subjects = `Matérias: ${checkSelected(document.querySelector('#label-content').value)}`;
+//   const rate = `Avaliação: ${checkSelected(document.querySelector('#label-rate').value)}`;
+//   const comments = `Observações: ${document.querySelector('#textarea').value}`;
+
+//   clearForm()
+
+//   const answers = [fullName, email, house, family, subjects, rate, comments];
+//   for (let index = 0; index < answers.length; index += 1) {
+//     const newElement = document.createElement('p');
+//     newElement.innerHTML = answers[index];
+//     evaluationForm.appendChild(newElement);
+//   }
+// });
+
+
+// const family = document.querySelectorAll('.family');
+// const checkedOptionFamily = '';
+// const familyChose = family.forEach(e => {
+//   if (e.checked) { 
+//    checkedOptionFamily = e.value;
+//   }
+// });
+
+// const module = document.getElementsByClass('subject');
+// const checkedOptionModule = ''
+// module.forEach(e => {
+//   if (e.checked) { 
+//    checkedOptionModule= e.value;
+//   }
+// });
+
+// const assessment = document.getElementsByName('rate');
+// const checkedOptionAssessment = ''
+// const assessmentChose = assessment.forEach(e => {
+//   if (e.checked) { 
+//    checkedOptionAssessment= e.value;
+//   }
+// });
+
